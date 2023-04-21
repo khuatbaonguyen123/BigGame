6c7cd9f27b1eda1ccec8a12267948dda3a8e0dad
khuatbaonguyen123 <anh10.14khuatbaonguyen@gmail.com>
2023-04-21 15:22:17 +0700

    Pieces

---
+++ TETRIS/Piece.h
@@ -0,0 +1,13 @@
+#ifndef Piece_h
+#define Piece_h
+
+class Piece
+{
+    public:
+
+	int GetBlockType		(int pPiece, int pRotation, int pX, int pY);
+	int GetXInitialPosition (int pPiece, int pRotation);
+	int GetYInitialPosition (int pPiece, int pRotation);
+};
+
+#endif // Piece_h
